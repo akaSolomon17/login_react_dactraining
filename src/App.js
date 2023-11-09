ca1bb3dd26b7874a2294c47d654a5dfc255c15c0
mafiavietquat <quochuy.4work@gmail.com>
2023-11-10 02:28:22 +0700

    UI edit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from "./logo.svg";
+// import logo from "./logo.svg";
 import "./App.css";
 import LoginForm from "./components/LoginForm";
 import { ChakraProvider } from "@chakra-ui/react";
--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,36 +1,109 @@
-import { Button, Input, Text, Box, Center, Flex } from "@chakra-ui/react";
+import {
+  Button,
+  Input,
+  CardBody,
+  Stack,
+  FormLabel,
+  Center,
+  Flex,
+  Card,
+  FormControl,
+  HStack,
+  Heading,
+  VStack,
+} from "@chakra-ui/react";
 import React from "react";
 
 const LoginForm = () => {
   return (
     <Flex
-      width={"100vw"}
-      height={"100vh"}
-      alignContent={"center"}
-      justifyContent={"center"}
-      bg={"blackAlpha.400"}
+      width="100vw"
+      height="100vh"
+      alignContent="center"
+      justifyContent="center"
+      bg="#dbdbdb"
     >
       <Center>
-        <Box
-          maxW="fit-content"
+        <Card
+          direction="column"
           maxH="fit-content"
+          h="480px"
+          w="460px"
           borderWidth="1px"
-          boxShadow="md"
-          bg="white"
+          boxShadow="lg"
+          bg="#f2f2f2"
+          borderRadius="lg"
+          borderColor="none"
         >
-          <Center>
-            <Text fontSize="6xl">WELCOME</Text>
-          </Center>
-          <Input type="email" placeholder="Email" />
-          <Input type="text" placeholder="Password" />
-          <Center>
-            <Button colorScheme="twitter">Login</Button>
-          </Center>
-          <Center display={"flex"}>
-            <Button colorScheme="facebook">Facebook</Button>
-            <Button colorScheme="red">Google</Button>
-          </Center>
-        </Box>
+          <CardBody>
+            <form>
+              <Stack spacing="4">
+                <FormControl>
+                  <VStack as="header" spacing="6" mt="8">
+                    <Heading
+                      as="h1"
+                      fontWeight="700"
+                      fontSize="4xl"
+                      letterSpacing="-0.5px"
+                      mb="8"
+                    >
+                      WELCOME
+                    </Heading>
+                  </VStack>
+                  <Input
+                    type="text"
+                    bg="white"
+                    borderColor="#d8dee4"
+                    size="lg"
+                    borderRadius="6px"
+                    placeholder="Email"
+                  />
+                </FormControl>
+                <FormControl>
+                  <HStack justify="space-between">
+                    <Button
+                      as="a"
+                      href="#"
+                      variant="link"
+                      size="xs"
+                      color="#0969da"
+                      fontWeight="500"
+                      mb={1}
+                    >
+                      Forgot password?
+                    </Button>
+                  </HStack>
+                  <Input
+                    type="password"
+                    bg="white"
+                    borderColor="#d8dee4"
+                    size="lg"
+                    borderRadius="6px"
+                    placeholder="Password"
+                  />
+                </FormControl>
+
+                <Button
+                  bg="#468FAF"
+                  color="white"
+                  size="lg"
+                  _hover={{ bg: "#568FAF" }}
+                  _active={{ bg: "#468FAF" }}
+                >
+                  Login
+                </Button>
+              </Stack>
+            </form>
+            <Center display="flex" justifyContent="space-between">
+              <Button colorScheme="facebook" size="lg" mt={4} w="200px">
+                Facebook
+              </Button>
+              <Button colorScheme="red" size="lg" mt={4} w="200px">
+                Google
+              </Button>
+            </Center>
+          </CardBody>
+        </Card>
       </Center>
     </Flex>
   );
